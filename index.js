4d9ce90a7cb695ab6aa9be5766af077946159232
Nathy-tech <mannathy5@gmail.com>
2024-11-17 09:53:24 -0800

    added 3 routes for the server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Route for /name
+app.get('/name', (req, res) => {
+  res.status(200).send('NATNAEL DAGNACHEW TEFERI');  
+});
+
+// Route for /hobby
+app.get('/hobby', (req, res) => {
+  const hobby = {
+    favoriteHobby: 'playing playstation'  
+  };
+  res.status(200).json(hobby);
+});
+
+// Route for /dream
+app.get('/dream', (req, res) => {
+  res.status(200).send('My dream is to become a software engineer who helps build innovative technology.');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
